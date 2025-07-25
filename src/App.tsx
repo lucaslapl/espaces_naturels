e7a774a6b51356c2d297c35ab35f6e4275fb1dfb
Intronium <lucas.laplanche77@gmail.com>
2025-07-25 16:51:36 +0200

    removed unused component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import './App.css'
 import Carte from './components/Carte';
-import AjoutLieu from './components/AjoutLieu';
 
 function App() {
   return (
     <div className="w-full mx-auto space-y-8">
-      {/* <AjoutLieu /> */}
       <Carte />
     </div>
   );
